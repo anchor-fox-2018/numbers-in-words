469a8587849638f72228dd7e3f02a0603dbb1d2f
Lundy Orlando <simplyeazy@gmail.com>
2018-09-03 19:36:42 +0700

    Release 2 First Commit Cape bikin if case

--- main.js
+++ main.js
@@ -16,18 +16,27 @@ function numberToWord(numbers) {
     else if (numbers < 1000) {
         result = result + numberToWord(numbers / 100) + " Ratus " + numberToWord(numbers % 100);
     }
-        else if (numbers < 2000) {
+    else if (numbers < 2000) {
         result = result + "Seribu " + numberToWord(numbers - 1000);
     }
     else if (numbers < 1000000000) {
         result = result + numberToWord(numbers / 1000) + " Ribu " + numberToWord(numbers % 1000);
-    }                   
+    }
     else if (numbers >= 1000000000) {
         result = result + numberToWord(numbers / 1000000) + " Juta " + numberToWord(numbers % 1000000);
     }
-
+    else if (numbers < 100000000000) {
+        result = result + numberToWord(numbers / 1000000000) + " Miliar " + numberToWord(numbers % 1000000000)
+    }
+    else if (numbers < 1000000000000) {
+        result = result + numberToWord(numbers / 1000000000000) + " Triliun " + numberToWord(numbers % 1000000000000)
+    }
+    else if (numbers < 10000000000000) {
+        result = result + numberToWord(numbers / 1000000000000000) + " Biliun " + numberToWord(numbers % 1000000000000000)
+    }
     return result;
 }
-for (let index = 0; index <= 1000000; index++) {
-    console.log(numberToWord(index))
+// cape bikin if casenya bro
+for (let index = 0; index <= 1000; index += 200) {
+    console.log(index)
 }
