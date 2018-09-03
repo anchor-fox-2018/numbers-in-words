2b7a564435e9152509e1c297e4e41596ceaa5f5b
Lundy Orlando <simplyeazy@gmail.com>
2018-09-03 19:20:02 +0700

    Release 1 First Commit

--- main.js
+++ main.js
@@ -16,7 +16,18 @@ function numberToWord(numbers) {
     else if (numbers < 1000) {
         result = result + numberToWord(numbers / 100) + " Ratus " + numberToWord(numbers % 100);
     }
+        else if (numbers < 2000) {
+        result = result + "Seribu " + numberToWord(numbers - 1000);
+    }
+    else if (numbers < 1000000000) {
+        result = result + numberToWord(numbers / 1000) + " Ribu " + numberToWord(numbers % 1000);
+    }                   
+    else if (numbers >= 1000000000) {
+        result = result + numberToWord(numbers / 1000000) + " Juta " + numberToWord(numbers % 1000000);
+    }
 
     return result;
 }
-
+for (let index = 0; index <= 1000000; index++) {
+    console.log(numberToWord(index))
+}
