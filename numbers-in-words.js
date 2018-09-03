5ac4811353c87c85499a93ac0567ce493ae752c7
days88 <yayahidayatullah@rocketmail.com>
2018-09-04 00:54:59 +0800

    Number In Word

--- numbers-in-words.js
+++ numbers-in-words.js
@@ -1,8 +1,8 @@
 function in_words(angka){
     var angkaPertama = String(angka);
     var kata = ['',' satu ',' dua ',' tiga ',' empat ',' lima ',' enam ',' tujuh ',' delapan ',' sembilan ',' sepuluh '];
-    var tingkatan = ['',' puluh ',' ratus ',' ribu ',' puluh ',' ratus ',,' puluh ',' ratus ',];
-    var awalanSatu = ['',' sepuluh ',' seratus ',' seribu ',' sepuluh ',' seratus '];
+    var tingkatan = ['','puluh','ratus','ribu','puluh','ratus',,'puluh','ratus',];
+    var awalanSatu = ['',' sepuluh',' seratus',' seribu',' sepuluh',' seratus'];
     var belasan = ['',' sebelas ',' dua belas ',' tiga belas ',' empat belas ',' lima belas ',' enam belas ',' tujuh belas ',' delapan belas ',' sembilan belas ']
     if(angkaPertama.length === 1){
         return ''+kata[Number(angkaPertama)]
@@ -17,6 +17,10 @@ function in_words(angka){
         }else{
             if(angkaPertama.length === 7){
                 return kata[Number(angkaPertama[0])]+' juta '+ in_words(angkaPertama.substring(1))
+            }else if(angkaPertama.length === 5 || angkaPertama.length === 6 && angkaPertama[1] === '0'){
+                return kata[Number(angkaPertama[0])]+tingkatan[angkaPertama.length-1]+ ' ribu'+ in_words(angkaPertama.substring(1))
+            }else if(angkaPertama.length === 8 || angkaPertama.length === 9 && angkaPertama[1] === '0'){
+                return kata[Number(angkaPertama[0])]+tingkatan[angkaPertama.length-1]+ ' juta'+ in_words(angkaPertama.substring(1))
             }else{
                 return kata[Number(angkaPertama[0])]+tingkatan[angkaPertama.length-1]+ in_words(angkaPertama.substring(1))
             }
@@ -25,3 +29,4 @@ function in_words(angka){
         
     } 
 }
+console.log(in_words(99000000))
